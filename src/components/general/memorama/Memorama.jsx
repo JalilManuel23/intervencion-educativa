d717dd8bb489550b6bc63da005a276a799d4f975
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-06-29 21:36:20 -0500

    Se agrega op de reiniciar juego y num de intentos

--- src/components/general/memorama/Memorama.jsx
+++ src/components/general/memorama/Memorama.jsx
@@ -4,41 +4,43 @@ import construirBaraja from './utils/construirBaraja';
 import { Carta } from './Carta';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 export const Memorama = () => {
 
-    const [baraja, setBaraja] = useState( construirBaraja() );
-    const [parejaSeleccionada, setParejaSeleccionada] = useState( [] );
-    const [estaComparando, setEstaComparando] = useState( false );
-    
-    const seleccionarCarta  = ( carta )  =>  { 
-        if(
-            estaComparando || 
+    const [baraja, setBaraja] = useState(construirBaraja());
+    const [parejaSeleccionada, setParejaSeleccionada] = useState([]);
+    const [estaComparando, setEstaComparando] = useState(false);
+    const [numeroDeIntentos, setNumeroDeIntentos] = useState(0);
+
+    const seleccionarCarta = (carta) => {
+        if (
+            estaComparando ||
             carta.fueAdivinada ||
-            parejaSeleccionada.indexOf( carta ) > -1
+            parejaSeleccionada.indexOf(carta) > -1
         ) {
             return;
         }
 
-        setParejaSeleccionada( [ ...parejaSeleccionada , carta ] );
+        setParejaSeleccionada([...parejaSeleccionada, carta]);
     }
 
     useEffect(() => {
-        if( parejaSeleccionada.length === 2 ) {
-            compararPareja( parejaSeleccionada );
+        if (parejaSeleccionada.length === 2) {
+            compararPareja(parejaSeleccionada);
         }
-    }, [ parejaSeleccionada ]);
+    }, [parejaSeleccionada]);
 
-    const compararPareja = ( pareja ) => { 
-        setEstaComparando( true );
+    const compararPareja = (pareja) => {
+        setEstaComparando(true);
 
         setTimeout(() => {
-            let [ carta1, carta2 ] = pareja;
+            let [carta1, carta2] = pareja;
             let newBaraja = baraja;
 
-            if( carta1.data === carta2.data ) {
+            if (carta1.data === carta2.data) {
                 newBaraja = newBaraja.map((carta) => {
-                    if( carta.data !== carta1.data ) {
+                    if (carta.data !== carta1.data) {
                         return carta;
                     }
 
@@ -46,27 +48,57 @@ export const Memorama = () => {
                 })
             }
 
-            setParejaSeleccionada( [] );
-            setBaraja( newBaraja );
-            setEstaComparando( false );
+            verificarSiHayGanador( newBaraja );
+
+            setNumeroDeIntentos( numeroDeIntentos + 1 ); 
+            setParejaSeleccionada([]);
+            setBaraja(newBaraja);
+            setEstaComparando(false);
         }, 1000);
     }
 
-    return(
+    const verificarSiHayGanador = (baraja) => {
+        if (
+            baraja.filter((carta) => !carta.fueAdivinada).length === 0
+        ) {
+            Swal.fire({
+                title: `¡Has ganado en ${ numeroDeIntentos } intentos!`,
+                showDenyButton: true,
+                confirmButtonText: `Jugar de nuevo`,
+                denyButtonText: `Cerrar`,
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    reiniciarJuego();
+                } else if (result.isDenied) {
+                    Swal.fire('Changes are not saved', '', 'info')
+                }
+            })
+        }
+    }
+
+    const reiniciarJuego = () => { 
+        setBaraja(construirBaraja());
+        setParejaSeleccionada([]);
+        setEstaComparando(false);
+        setNumeroDeIntentos(0);
+    }
+
+    return (
         <div className="memorama container d-flex flex-column align-items-center mt-5">
             <h1>Título</h1>
-            
+
             <div class="row-cartas mt-3">
                 {
-                    baraja.map( (carta, index) => {
+                    baraja.map((carta, index) => {
                         const estaSiendoComparada = parejaSeleccionada.indexOf(carta) > -1;
                         return (
-                            <Carta 
-                                key={ index } 
-                                info={ carta } 
-                                estaSiendoComparada= { estaSiendoComparada }
-                                seleccionarCarta= { () => seleccionarCarta( carta ) }
-                                fueAdivinada={ carta.fueAdivinada }
+                            <Carta
+                                key={index}
+                                info={carta}
+                                estaSiendoComparada={estaSiendoComparada}
+                                seleccionarCarta={() => seleccionarCarta(carta)}
+                                fueAdivinada={carta.fueAdivinada}
                             />
                         )
                     })
