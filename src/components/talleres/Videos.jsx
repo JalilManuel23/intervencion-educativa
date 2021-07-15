a7a29d6180a9791625ccb1089fccb4526709ec0a
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-07-15 18:55:31 -0500

    Modificaciones en estilos

--- src/components/talleres/Videos.jsx
+++ src/components/talleres/Videos.jsx
@@ -25,21 +25,21 @@ export const Videos = ({
                 <p className="display-6 mt-2 text-white">{texto} </p>
                 {
                     (imagen) &&
-                    <img src={imagen} />
+                    <img src={imagen} className="img-videos" />
                 }
-                <div class="d-grid gap-2 col-6 mx-auto">
-                    <Link
-                        className="btn btn-warning p-2  text-white btn-lg mt-5"
-                        to={enlace}
-                        onClick={
-                            () => {
-                                setStopAudio(sound);
-                            }
+                
+                <Link
+                    className="btn btn-primary btn-lg m-5"
+                    to={enlace}
+                    onClick={
+                        () => {
+                            setStopAudio(sound);
                         }
-                    >
-                        <span className="display-6">{textoBtn}</span>
-                    </Link>
-                </div>
+                    }
+                >
+                    <span className="display-6">{textoBtn}</span>
+                </Link>
+              
             </div>
             <AudioPlayer sound={sound} />
         </div>
