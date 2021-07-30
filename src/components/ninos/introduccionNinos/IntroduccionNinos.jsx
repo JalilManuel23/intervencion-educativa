6a5cc65ebfc1abe49675c2061538a7c8b72057fd
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-07-30 10:45:00 -0500

    Se solucionan problemas responsive en intro niños

--- src/components/ninos/introduccionNinos/IntroduccionNinos.jsx
+++ src/components/ninos/introduccionNinos/IntroduccionNinos.jsx
@@ -20,17 +20,17 @@ export const IntroduccionNinos = () => {
     return (
         <div className="intro-ninos animate__animated animate__fadeIn">
             <div className="container">
-                <div className="row d-flex pt-5">
-                    <div className="img-container-intro-ninos col-md-5 col-12 d-flex mb-5">
+                <div className="row d-flex pt-5 justify-content-center justify-content-lx-between">
+                    <div className="img-container-intro-ninos col-xl-5 col-8 d-flex mb-5 align-items-center" >
                         <img 
                             src={ imagenes.nina }
                             alt="niña"
                             className="img-intro-ninos animate__animated animate__backInDown"
                         />
                     </div>
-                    <div className="info-intro-ninos d-flex flex-column justify-content-between align-items-center col-12 col-md-6 animate__animated animate__bounceIn">
+                    <div className="info-intro-ninos d-flex flex-column align-items-center col-12 col-xl-6 animate__animated animate__bounceIn">
                         <p className="tit-intro-ninos">¿Por qué todos hablan de algo que se llama CORONAVIRUS?</p>
-                        <p className="contenedor-intro">CORONAVIRUS es un grupo de virus que pueden hacer que las personas se sientan enfermas.</p>
+                        <p className="contenedor-intro mt-3">CORONAVIRUS es un grupo de virus que pueden hacer que las personas se sientan enfermas.</p>
                     </div>
                 </div>
                 <Control 
