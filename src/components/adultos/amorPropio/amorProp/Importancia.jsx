c3f7aeb5849f02319f5c8d61b7897a8d9d62d107
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-08-07 10:26:51 -0500

    Se realizan correciones

--- src/components/adultos/amorPropio/amorProp/Importancia.jsx
+++ src/components/adultos/amorPropio/amorProp/Importancia.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Videos } from '../../../talleres/Videos';
 import audio from "../../../../assets/sounds/adultos_amor_propio/video_3.mp3";
 import { rutasAmorPropio } from '../rutasAmorPropio'
+import imagenes from '../../../../assets/images';
 
 export const Importancia = () => {
     return (
         <Videos 
             titulo="¿Por qué es tan importante tener mucho amor propio?"
-            texto="Descúbrelo en el siguiente video:"
+            // texto="Descúbrelo en el siguiente video:"
+            imagen={ imagenes.feliz }
             enlace="/adultos/amor-propio/videos/importancia"
             sonido={ audio }
             rutas={ rutasAmorPropio }
